b185e662794fbc530b6b4376e93ea95ed75bc62c
WuMianZhi <1427207878@qq.com>
2021-11-26 18:58:50 +0800

    Models ran on raspberry pi

---
+++ tts_on_raspberry/build/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wu/ttsdemo/demo/main.cpp" "/home/wu/ttsdemo/build/CMakeFiles/demo.dir/demo/main.cpp.o"
+  "/home/wu/ttsdemo/src/MelGenerateTF.cpp" "/home/wu/ttsdemo/build/CMakeFiles/demo.dir/src/MelGenerateTF.cpp.o"
+  "/home/wu/ttsdemo/src/TTSBackend.cpp" "/home/wu/ttsdemo/build/CMakeFiles/demo.dir/src/TTSBackend.cpp.o"
+  "/home/wu/ttsdemo/src/TTSFrontend.cpp" "/home/wu/ttsdemo/build/CMakeFiles/demo.dir/src/TTSFrontend.cpp.o"
+  "/home/wu/ttsdemo/src/TfliteBase.cpp" "/home/wu/ttsdemo/build/CMakeFiles/demo.dir/src/TfliteBase.cpp.o"
+  "/home/wu/ttsdemo/src/VocoderTF.cpp" "/home/wu/ttsdemo/build/CMakeFiles/demo.dir/src/VocoderTF.cpp.o"
+  "/home/wu/ttsdemo/src/VoxCommon.cpp" "/home/wu/ttsdemo/build/CMakeFiles/demo.dir/src/VoxCommon.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BAKER"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib"
+  "../lib/flatbuffers/include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tts_on_raspberry/build/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/demo/main.cpp.o"
+  "CMakeFiles/demo.dir/src/MelGenerateTF.cpp.o"
+  "CMakeFiles/demo.dir/src/TTSBackend.cpp.o"
+  "CMakeFiles/demo.dir/src/TTSFrontend.cpp.o"
+  "CMakeFiles/demo.dir/src/TfliteBase.cpp.o"
+  "CMakeFiles/demo.dir/src/VocoderTF.cpp.o"
+  "CMakeFiles/demo.dir/src/VoxCommon.cpp.o"
+  "demo"
+  "demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
